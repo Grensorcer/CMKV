1c49d4348e1711fb0b522f9b1fd7c826859cbd09
lukas.rabier <lukas.rabier@epita.fr>
2021-12-08 21:04:26 +0100

    [SIMULANNEAL] implemented temperature binary search with standard deviations

--- simulated_annealing.cc
+++ simulated_annealing.cc
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <algorithm>
 #include <memory>
+#include <fstream>
 #include <random>
+#include <vector>
 #include <cmath>
 #include "board.hh"
 
@@ -29,53 +32,86 @@ protected:
     DistributionType distribution_;
 };
 
-bool simulated_annealing(Board &board, float decrease_factor, unsigned max_iterations=1000000, unsigned stuck_max=1000, float initial_temperature = 20.0)
+bool simulated_annealing(Board &board, float decrease_factor,
+                         unsigned max_iterations = 10000,
+                         float max_temperature = 100.0,
+                         float min_temperature = 1.0)
 {
+    double min_std = 1.;
+    double max_std = 1000.;
+
     auto gen = std::make_shared<std::mt19937>(std::random_device{}());
-    auto uniform_index =
-        Distribution<std::uniform_int_distribution<>>(gen, 0, board.count() - 1);
+    auto uniform_index = Distribution<std::uniform_int_distribution<>>(
+        gen, 0, board.count() - 1);
     auto uniform_distrib =
         Distribution<std::uniform_real_distribution<float>>(gen, 0, 1);
 
     board.random_initialize();
-    unsigned stuck = 0;
-    float sigma = initial_temperature;
-    unsigned unstuck_count = 0;
+    double sigma = (max_temperature + min_temperature) / 2.;
+    auto errors = std::vector<double>();
+    errors.reserve(max_iterations);
 
-    unsigned i;
-    for (i = 0; i < max_iterations && board.score() != 0; i++)
+    unsigned j;
+    for (j = 0; j < 10000 && sigma <= max_temperature
+         && sigma >= min_temperature && board.score() != 0;
+         ++j)
     {
-        long old_score = board.score();
-        unsigned index1 = uniform_index();
-        unsigned index2 = index1;
-        while (index2 == index1)
-            index2 = uniform_index();
-        board.swap(index1, index2);
-        long new_score = board.score();
-        auto tmp = std::exp((old_score - new_score) / (double)(sigma));
-        if (uniform_distrib() > tmp)
+        // std::cout << "Min: " << min_temperature << " Max: " <<
+        // max_temperature
+        //           << " Tmp: " << sigma << std::endl;
+        for (unsigned i = 0; i < max_iterations && board.score() != 0; ++i)
         {
-            stuck++;
-            if (stuck > stuck_max)
-            {
-                if (unstuck_count == 50)
-                {
-                    sigma = initial_temperature;
-                    unstuck_count = 0;
-                }
-                else
-                {
-                    sigma += 1.0;
-                    unstuck_count++;
-                }
-                stuck = 0;
-            }
+            long old_score = board.score();
+            unsigned index1 = uniform_index();
+            unsigned index2 = index1;
+            while (index2 == index1)
+                index2 = uniform_index();
             board.swap(index1, index2);
+            long new_score = board.score();
+            auto tmp = std::exp((old_score - new_score) / sigma);
+
+            if (uniform_distrib() > tmp)
+                board.swap(index1, index2);
+
+            sigma *= 1 - decrease_factor;
+            errors.emplace_back(tmp > 1 ? 1 : tmp);
         }
-        sigma *= 1 - decrease_factor;
-        std::cout << sigma << " old: " << old_score << " new: " << new_score << " " << tmp << std::endl;
+
+        double mean = std::accumulate(errors.begin(), errors.end(), 0.)
+            / (double)errors.size();
+        // std::cout << mean << std::endl;
+
+        std::transform(errors.begin(), errors.end(), errors.begin(),
+                       [&](double x) { return (x - mean) * (x - mean); });
+
+        /*
+        for (auto v : errors)
+            std::cout << v << " ";
+        std::cout << std::endl;
+        */
+
+        double std =
+            std::sqrt(std::accumulate(errors.begin(), errors.end(), 0.));
+        auto expected_std = (min_std + max_std) / 2.;
+        // std::cout << std << std::endl;
+
+        if (std <= expected_std)
+        {
+            max_std = expected_std;
+            max_temperature = (min_temperature + max_temperature) / 2;
+        }
+        else
+        {
+            min_std = expected_std;
+            min_temperature = (min_temperature + max_temperature) / 2;
+        }
+
+        sigma = (min_temperature + max_temperature) / 2.;
+        errors.clear();
     }
-    std::cout << i;
+
+    std::cout << "Iterations: " << j << " Temperature: " << sigma
+              << " Deviation: " << (min_std + max_std) / 2. << std::endl;
     return board.score() == 0;
 }
 
@@ -96,6 +132,9 @@ int main(int argc, char **argv)
         std::cout << "Game state: " << (win ? "Solved" : "Unsolved")
                   << std::endl
                   << b;
+
+        auto out = std::ofstream(argv[2]);
+        out << b;
     }
     return return_code;
 }
